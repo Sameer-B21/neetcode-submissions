accc297068012127f14d099b60e97ed86635d5ba
Sameer Bandha <147968242+Sameer-B21@users.noreply.github.com>
2026-08-14 18:29:31 -0400

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        alp = 'abcdefghijklmnopqrstuvwxyz0123456789'
+        temp = s.lower()
+        i, j = 0, len(temp)-1
+        while i<=j:
+            if temp[i] not in alp:
+                i+=1
+                continue
+            if temp[j] not in alp:
+                j-=1
+                continue
+            print(i, j)
+            if temp[i]!=temp[j]:
+                print((temp[i], temp[j]))
+                return False
+            i+=1
+            j-=1
+        return True
+        
